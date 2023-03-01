04650942a2ccca2a744a700521b39ba97c168dcc
georgespicy <kolawolegeorge28743@gmail.com>
2023-03-01 23:57:07 +0100

    pulling datail page from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 function ProductScreen() {
+    const [product, setProduct] = useState([])
     const { id } = useParams();
-    const product = products.find((p) => p._id == id);
+    useEffect(() => {
+        async function fetchproduct() {
+            const { data } = await axios.get(`/api/product/${id}`)
+            setProduct(data)
+        }
+        fetchproduct()
+    }, [])
     return (
         <div>
+            <h1>product screen</h1>
             <Link to='/' className='btn btn-light my-3'>Go back</Link>
             <Row>
                 <Col md={6}>
